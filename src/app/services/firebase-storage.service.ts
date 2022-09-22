e2699d8bbd1771bd592b3ff7d6b8d01256af4369
izzie1A <47637788+izzie1A@users.noreply.github.com>
2022-09-22 07:12:47 +0100

    jhb

--- src/app/services/firebase-storage.service.ts
+++ src/app/services/firebase-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { getDownloadURL, getMetadata, getStorage, listAll, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
+import {FirebaseCloudstoreService} from 'src/app/services/firebase-cloudstore.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,7 @@ import { getDownloadURL, getMetadata, getStorage, listAll, ref, uploadBytes, upl
 export class FirebaseStorageService {
   storage: any;
   childArray: any;
-  constructor() {
-    
+  constructor(fcloud:FirebaseCloudstoreService) {
   };
 
 
@@ -18,10 +18,10 @@ export class FirebaseStorageService {
     const mountainsRef = ref(this.storage, dir);
     return mountainsRef;
   }
-  async readRoot(input:string){
+  async readRoot(input: string) {
     const storage = getStorage();
     const rootRef = ref(storage, 'root/user/test/image/');
-    let output:any[] = new Array();
+    let output: any[] = new Array();
     // Find all the prefixes and items.
     await listAll(rootRef)
       .then((res) => {
@@ -34,26 +34,26 @@ export class FirebaseStorageService {
       }).catch((error) => {
         // Uh-oh, an error occurred!
       });
-      console.log(this.childArray);
-      return output
+    console.log(this.childArray);
+    return output
   }
 
-  async uploadFile(refDir: any, input: any) {
-    let uploadTask =  uploadBytesResumable(this.getRef(refDir), input);
-    await uploadTask.on('state_changed',(snapshot) => {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log(snapshot.bytesTransferred +'/ '+snapshot.totalBytes+'Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
-            break;
-          case 'running':
-            console.log('Upload is running');
-            break;
-        }
-      },
+  async uploadFile(refDir: string, input: any) {
+    let uploadTask = uploadBytesResumable(this.getRef(refDir), input);
+    await uploadTask.on('state_changed', (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log(snapshot.bytesTransferred + '/ ' + snapshot.totalBytes + 'Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+      }
+    },
       (error) => {
         alert(error.message);
       },
@@ -66,9 +66,9 @@ export class FirebaseStorageService {
     );
   }
 
-  async readFile(refDir: any) {
+  async readFile(refDir: string) {
     let output = '';
-    await getDownloadURL( this.getRef(refDir))
+    await getDownloadURL(this.getRef(refDir))
       .then((url) => {
         output = url;
       })
@@ -78,18 +78,24 @@ export class FirebaseStorageService {
     console.log(output)
     return output
   }
-  async readFileMetadata(refDir: any) {
-    let output:any;
+
+  async readFileMetadata(refDir: string) {
+    let output: any;
     await getMetadata(this.getRef(refDir))
-    .then((metadata) => {
-      output = metadata;
-      // Metadata now contains the metadata for 'images/forest.jpg'
-    })
-    .catch((error) => {
-      // Uh-oh, an error occurred!
-    });
+      .then((metadata) => {
+        output = metadata;
+        // Metadata now contains the metadata for 'images/forest.jpg'
+      })
+      .catch((error) => {
+        // Uh-oh, an error occurred!
+      });
     console.log(output)
     return output
   }
+
+  uploadObject(refDir: string, input: any) {
+    this.uploadFile(refDir, input);
+    this.fcloud.write(refDir, refDir, input)
+  }
 }
-  
+
